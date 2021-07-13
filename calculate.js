a4be4ea5059286a4ce3cc7767a2f9ae48477ab68
JOANITA-51 <nakityojoanita51@gmail.com>
2021-07-13 11:11:54 +0300

    Exporting morethan one function and testing them using the destructuring method. Destructure helps us to create shorter variables and extract data from those variables

--- calculate.js
+++ calculate.js
@@ -1,12 +1,35 @@
 let sum = (num1, num2) =>{
-   // if(typeof a == 'undefined') return false
-   // if(typeof b == 'undefined') return false
-   if (typeof num1 !== 'number' || typeof num2 !== 'number') return false
+      if (typeof num1 !== 'number' || typeof num2 !== 'number') return false
 
    return num1 + num2 ;
 }
 console.log(sum(5,6))
-//console.log(sum (o,6)) //This can be very hard to troubleshoot because thhe system compiler won't return clear error message
-//npconsole.log(sum(10, '90'))
 
-module.exports = sum
+let subtract = (num1 , num2) =>{
+   if (typeof num1 !== 'number' || typeof num2 !== 'number') return false
+
+   return num1 - num2
+}
+
+module.exports = {
+   sum,
+   subtract
+}
+
+/**
+ * Example of destructuring
+ */
+const person ={
+   'firstname':'Joanita',
+   'lastname':'Nakityo',
+   'middlename':'Nita',
+   'town':'Lyantonde'
+}
+//Destructured variables
+const {firstname, lastname} = person
+
+console.log(firstname);
+
+//Name destructured variables
+console.log(person["firstname"]);
+console.log(person.firstname);
