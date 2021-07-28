b21ccab8ad89c70529148272afbe8f78e56ea956
JOANITA-51 <nakityojoanita51@gmail.com>
2021-07-28 11:56:05 +0300

    validating input length

--- session 8/src/validator.js
+++ session 8/src/validator.js
@@ -1,10 +1,15 @@
   
-const CHECK_EMPTY = (event) => {
-
-    if (event.target.value == '' || event.target.value == null) {
-        event.target.classList.add('is-invalid')
+const CHECK_EMPTY = (event, length=8) => {
+    const ELEMENT = event.target
+    if (ELEMENT.value == '' || ELEMENT.value == null) {
+        ELEMENT.classList.add('is-invalid')
         // alert('Field required')
         // console.log(event.target)
+        return false
+    }
+
+    if (ELEMENT.value.length< length){
+        ELEMENT.classList.add("is-invalid");
         return false
     }
 
