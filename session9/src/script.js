c7cd9c9d6fe1c87de74dccd7fa746f28cf5d699d
JOANITA-51 <nakityojoanita51@gmail.com>
2021-07-29 12:25:44 +0300

    the fetch command in APIs

--- session9/src/script.js
+++ session9/src/script.js
@@ -1,20 +1,21 @@
 const TABLE_DATA = document.getElementById('table-data')
 let generatedHTMLRows =''
+//explain the fetch function
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) =>response.json())
   .then((json)=>{
       let todos = json
 
       if (todos && todos.length>0){
-          todos.forEach(todo => {
+          todos.forEach(todo => { //explain what this does
               generatedHTMLRows += `
               <tr>
                 <td>${todo.id}</td>
-                <td>${todo.userid}</td>
+                <td>${todo.userId}</td>
                 <td>${todo.title}</td>
                 <td>${todo.completed? "Completed":"Incomplete"}</td>
                </tr>  
-              `;
+              `;//explain this block of code
               
           });
       }
