e4a5e080ebf90ffc743c0084ee6bfb4603822d62
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-17 12:29:03 +0300

    arithmetic operations and mtd 2: Using exports

---
+++ session10/arithmetic-operations.js
@@ -0,0 +1,13 @@
+
+exports.customMath = {
+    name: 'custom Math',
+    add: (num1, num2) => num1 + num2,
+    subtract : (num1, num2) => num1 - num2,
+    //product: (numbers =[])=>{
+    //    return numbers.reduce((accumulator, currentValue)=> accumulator * currentValue)
+    //}
+    product : (numbers = []) =>
+        numbers.reduce((accumulator, currentValue) => accumulator * currentValue)
+
+    
+}
---
+++ session10/usage.js
@@ -0,0 +1,13 @@
+const customMath = require('./arithmetic-operations').customMath
+
+//Add numbers 2 + 5 
+console.log(customMath.add(2,5))
+
+//Subtract 3-2 
+console.log(customMath.subtract(3, 2))
+
+//Subtract 2-5
+console.log (customMath.subtract(2, 5))
+
+//Product of [2,2,2]
+console.log (customMath.product([2, 2, 2]))
