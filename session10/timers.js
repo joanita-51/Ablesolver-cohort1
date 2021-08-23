c6621cce717b2bcb3d28b3e9bfdd7a19792d35b6
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-23 12:47:03 +0300

    setTimeout with params

--- session10/timers.js
+++ session10/timers.js
@@ -5,3 +5,8 @@ setTimeout(()=>{
 setTimeout(()=>{
     console.log('Run after 50 milliseconds')
 }, 50);
+
+// setTimeout with params
+let greeting = (name, msg)=>console.log(`${msg} ${name}`)
+setTimeout(greeting,1000, 'David', 'Hola')
+setTimeout(greeting,500,'Anita', 'Agandi')
