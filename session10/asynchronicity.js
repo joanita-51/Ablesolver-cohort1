9f45d948547b75d5cc6389179818e960c3001a95
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-25 12:19:30 +0300

    chaining promises

--- session10/asynchronicity.js
+++ session10/asynchronicity.js
@@ -29,26 +29,41 @@
 // //Invoke the checker.
 // CHECK_IF_DONE()
 
-const FS = require ('fs')
+// const FS = require ('fs')
 
-const READ_FILE = (fileName) => {
-  //Create and return a promise
-  return new Promise ((resolve, reject) => {
-    //use the readFilename, encoding, callback) method of the fs module
-    FS.readFile(fileName, 'utf8', (err, data) => { //Specify the encoding to get a readable buffer
-      if (err){
-        reject (err) //Reject the promise
-        return //And stop code with data
-      }
-      resolve (data) //Resolve with data
-    })
-  })
+// const READ_FILE = (fileName) => {
+//   //Create and return a promise
+//   return new Promise ((resolve, reject) => {
+//     //use the readFilename, encoding, callback) method of the fs module
+//     FS.readFile(fileName, 'utf8', (err, data) => { //Specify the encoding to get a readable buffer
+//       if (err){
+//         reject (err) //Reject the promise
+//         return //And stop code with data
+//       }
+//       resolve (data) //Resolve with data
+//     })
+//   })
+// }
+
+// //Use the promise above
+// READ_FILE('./readme.md')
+// .then(data => console.log(data))
+// .catch(error => console.log(error))
+
+const fetch = require('node-fetch')
+const STATUS = response =>{
+  let {status, statusText} = response
+  if(status >= 200 && status < 300)
+    return Promise.resolve(response)
+  return Promise.reject (new Error (statusText))  
 }
 
-//Use the promise above
-READ_FILE('./readme.md')
-.then(data => console.log(data))
-.catch(error => console.log(error))
+const JSON = response => response.json()
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(STATUS)
+  .then(JSON)
+  .then((data) => {
+    console.log(data);
 
-
-
+  })
+  .catch((error) => console.log(error));
