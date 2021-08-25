55df9a2a9dc5fb0e80953a69fd6481e766f6caf4
JOANITA-51 <nakityojoanita51@gmail.com>
2021-08-25 11:43:00 +0300

    reading a file

--- session10/asynchronicity.js
+++ session10/asynchronicity.js
@@ -1,33 +1,54 @@
-const { reject } = require("lodash")
+// const { reject } = require("lodash")
 
-/**
- * Promises are alternatives to callback
- * Because callbacks can make code look complidcated hence had to maintain
- * They are 2 parts to them.
- * 1. Definition of the promise
- * 2. Usage of the promise/consuming
- */
-const DONE = true
-// Definition of the promise
-const IS_IT_DONE = new Promise((resolve, reject) => { 
-  const SUCCESS = 'Promise resolved '
-  if (DONE) {
-    resolve(SUCCESS) //Was passed as an argument to the constructor
-  }
-  else
-  {
-    const FAILURE = 'Promise failed'
-    reject(FAILURE); //was passed as an argumwnt to the constructor
-  }
-})
-//Usage
-const CHECK_IF_DONE = () => {
-  IS_IT_DONE 
-  .then (result => console.log(result))
-  .catch(error => console.log (error))
+// /**
+//  * Promises are alternatives to callback
+//  * Because callbacks can make code look complidcated hence had to maintain
+//  * They are 2 parts to them.
+//  * 1. Definition of the promise
+//  * 2. Usage of the promise/consuming
+//  */
+// const DONE = true
+// // Definition of the promise
+// const IS_IT_DONE = new Promise((resolve, reject) => { 
+//   const SUCCESS = 'Promise resolved '
+//   if (DONE) {
+//     resolve(SUCCESS) //Was passed as an argument to the constructor
+//   }
+//   else
+//   {
+//     const FAILURE = 'Promise failed'
+//     reject(FAILURE); //was passed as an argumwnt to the constructor
+//   }
+// })
+// //Usage
+// const CHECK_IF_DONE = () => {
+//   IS_IT_DONE 
+//   .then (result => console.log(result))
+//   .catch(error => console.log (error))
+// }
+// //Invoke the checker.
+// CHECK_IF_DONE()
+
+const FS = require ('fs')
+
+const READ_FILE = (fileName) => {
+  //Create and return a promise
+  return new Promise ((resolve, reject) => {
+    //use the readFilename, encoding, callback) method of the fs module
+    FS.readFile(fileName, 'utf8', (err, data) => { //Specify the encoding to get a readable buffer
+      if (err){
+        reject (err) //Reject the promise
+        return //And stop code with data
+      }
+      resolve (data) //Resolve with data
+    })
+  })
 }
-//Invoke the checker.
-CHECK_IF_DONE()
+
+//Use the promise above
+READ_FILE('./readme.md')
+.then(data => console.log(data))
+.catch(error => console.log(error))
 
 
 
