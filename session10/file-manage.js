60a73b1891b9e9cafb0fd749a5424cb46f56fd17
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-09 10:24:13 +0300

    writting to a file

--- session10/file-manage.js
+++ session10/file-manage.js
@@ -5,3 +5,14 @@ fs.open ('./.gitignore','r',(err,fd)=>{
 fs.open ('./.gitignore','r+',(err,fd)=>{
     console.log(fd)
 })
+fs.open ('./logs','w+',(err,fd)=>{
+    console.log(fd)
+})
+try {
+    const fd = fs.openSync('test.txt', 'w+')
+    console.log('openSync' +fd)
+    fs.closeSync(fd)
+    console.log('After closing' +fd)
+} catch (err) {
+    console.error(err)
+}
