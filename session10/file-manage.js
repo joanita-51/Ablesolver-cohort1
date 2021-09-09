0bef8322d0429514e54346c33ec349fcb1bbd612
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-09 11:39:39 +0300

    solution to the quizz

--- session10/file-manage.js
+++ session10/file-manage.js
@@ -16,3 +16,13 @@ try {
 } catch (err) {
     console.error(err)
 }
+/**
+ * Quizz add the description of fs.open or fs. openSync file nodes below.
+ * File reading nodes.
+ * r, r+, w+, a and a+
+ * r:  open the file for reading. The file is not created if not existing
+ * r+:open the file for reading and writing, if file don't exist it won't be created.
+ * w+:open the file for reading and writing, positioning the content being written at the beginning of the file. The file is created if not existing
+ * a: open the file for writing, positioning the stream(content) at the end of the file. The file is created if not existing
+ * a+: open the file for reading and writing, positioning the stream(content) at the end of the file. The file is created if not existing
+ */
