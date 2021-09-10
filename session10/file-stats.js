f75c8cb9d2d2d7cffcdee06fab604f6160563336
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-10 10:17:52 +0300

    synchronous way

--- session10/file-stats.js
+++ session10/file-stats.js
@@ -1,11 +1,20 @@
-const fs = require ('fs')
-fs.stat('test.txt', (err,stats)=>{
-    if (err) {
-        console.error(err)
-        return
-    }
+// const fs = require ('fs')
+// fs.stat('test.txt', (err,stats)=>{
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
 
+//     console.log(stats)
+//     process.stdout.write(stats)
+// })
+
+const fs = require('fs')
+try{
+    const stats = fs.statSync('test.txt')
     console.log(stats)
-    process.stdout.write(stats)
-})
+} catch (err){
+    console.error(err)
+}
 
+console.log('After the stats')
