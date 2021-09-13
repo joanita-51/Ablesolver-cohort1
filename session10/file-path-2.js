eec3d0bf805a32cd17555fda6adf99dd0d6a7fc9
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-13 09:55:42 +0300

    creating a file path

---
+++ session10/file-path-2.js
@@ -0,0 +1,5 @@
+const path = require ('path')
+
+const user = 'Anita'
+const PATH = path.join('/','Users',user,'logs.txt')
+console.log(PATH)
--- session10/file-path.js
+++
@@ -1,8 +0,0 @@
-const path = require ('path')
-
-const notes = '/users/joe/notes.txt'
-
-console.log(path.dirname (notes))
-console.log(path.basename (notes))
-console.log(path.extname (notes))
-console.log(path.basename (notes, path.extname(notes)))
