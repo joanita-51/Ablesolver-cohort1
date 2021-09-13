6f97b1802d24f5004687a07f6eb9c5e774dad8d3
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-13 10:04:59 +0300

    getting the absolute path for the file

--- session10/file-path-2.js
+++ session10/file-path-2.js
@@ -3,3 +3,10 @@ const path = require ('path')
 const user = 'Anita'
 const PATH = path.join('/','Users',user,'logs.txt')
 console.log(PATH)
+
+//Get absolute path of the file
+const absPath = path.resolve('file.txt')
+console.log (absPath)
+
+const absPath2 = path.resolve('tmp','file.txt')
+console.log (absPath2)
