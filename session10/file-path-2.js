c8f14d63ba362b338051cd263eb7a660c5ca5edc
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-13 10:08:51 +0300

    adding more parameters while using  path.join() method

--- session10/file-path-2.js
+++ session10/file-path-2.js
@@ -1,8 +1,13 @@
 const path = require ('path')
 
 const user = 'Anita'
+//Creating a path using path.join (separator, ...n)
+
 const PATH = path.join('/','Users',user,'logs.txt')
 console.log(PATH)
+
+const PATH2 = path.join('/','Users',user,'option-dir','opt2','logs.txt')
+console.log(PATH2)
 
 //Get absolute path of the file
 const absPath = path.resolve('file.txt')
