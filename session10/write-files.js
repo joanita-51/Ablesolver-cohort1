fd2a98270417fde80409719a9440f2f3998a3e82
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-17 19:41:29 +0300

    writing file synchronously

--- session10/write-files.js
+++ session10/write-files.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs')
 const content = 'This is written using NodeJS fs.writeFile() API'// You can also fetch this content from somewhere else
 //Asynchronous 
@@ -10,3 +11,22 @@ fs.writeFile('test1.txt',content, err=>{
     console.log ('Yes, we did it!')
     console.log ('Content has written to the file successfully!')
 })
+*/
+
+//Synchronous
+const fs = require('fs')
+const path = require('path')
+const filepath = path.resolve('test2.txt')
+
+let content = 'This is written synchronously and the path has been generated with path module. '
+
+try{
+    content = `${content}\n ${filepath}`
+
+    const data = fs.writeFileSync(filepath, content)
+    //file written successfully
+    console.log (`Yeap!\n You have written to the file.`)
+}catch (err){
+    console.error(err)
+}
+
