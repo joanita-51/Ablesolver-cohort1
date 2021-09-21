d6e3abe1ce17e6d05a9668b69a2b1139417194ab
JOANITA-51 <nakityojoanita51@gmail.com>
2021-09-21 09:40:30 +0300

    cleaning the branch

--- session10/http-module.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-// Get informationa about the http module
-console.log(http)
-
-//Output all the http STATUS_CODES and descriptions
-console.log(http.STATUS_CODES)
-
-//Output all the http Methods
-console.log(http.METHODS)
